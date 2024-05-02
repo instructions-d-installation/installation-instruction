462200a2b026c22c81f86daae08c27ea879bb057
Adam McKellar <dev@mckellar.eu>
2024-05-02 19:49:54 +0200

    Added pytorch example. Added example script.

---
+++ examples/pytorch/validate_user_input_with_schema_and_build_instruction.py
@@ -0,0 +1,46 @@
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper, load
+except ImportError:
+    from yaml import Loader, Dumper
+
+from jsonschema import validate
+
+from jinja2 import Template
+
+
+
+def load_yml(path: str) -> dict:
+    with open(path, 'r') as file:
+        input_str = file.read()
+        
+    return load(input_str, Loader=Loader)
+
+def load_template(path: str) -> Template:
+    with open(path, 'r') as file:
+        template_str = file.read()
+
+    return Template(template_str)
+
+
+
+
+schema = load_yml('instruction_pytorch.schema.yml')
+input = load_yml('potential_user_input.yml')
+#bad_input = load_yml('bad_user_input.yml')
+
+
+
+print("Test valid input.")
+validate(input, schema)
+print("It worked!")
+
+
+# print("Test invalid input.")
+# validate(bad_input, schema)
+# print("It worked!")
+
+
+template = load_template("instruction_pytorch.md.jinja")
+
+instructions = template.render(input)
+print(instructions)
