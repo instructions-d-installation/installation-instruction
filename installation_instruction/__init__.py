5935b1d3e838c42a55d99afcbfb033940aaf64da
Adam McKellar <dev@mckellar.eu>
2024-05-10 17:21:19 +0200

    Added class import in init. Removed example code from cookiecutter template.

--- installation_instruction/__init__.py
+++ installation_instruction/__init__.py
@@ -18,10 +18,5 @@ from importlib import metadata
 __version__ = metadata.version(__package__)
 del metadata
 
-def add_one(x: int):
-    """An example function that increases a number
+from installation_instruction.installation_instruction import InstallationInstruction
 
-    :param x: The input parameter to increase
-    :return: The successor of the given number
-    """
-    return x + 1
