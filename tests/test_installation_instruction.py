406eb8a2c84b6e6bddd513946621ef2dcb3b9647
TimoEg <TimoEge@online.de>
2024-05-20 15:53:06 +0200

    Pytest overhaul. added contest file and folder for test data. changed pytestsfiles for instalation_instruction and get_flags_and_options_from_schema

--- tests/test_installation_instruction.py
+++ tests/test_installation_instruction.py
@@ -13,53 +13,39 @@
 # limitations under the License.
 
 
-from pytest import raises
+import pytest
 
 from installation_instruction.installation_instruction import InstallationInstruction
 
-def test_validate_and_render_pytorch():
-    valid_user_input = {
-        "build": "preview",
-        "os": "win",
-        "package": "pip",
-        "compute_platform": "ro60"
-    }
+@pytest.mark.parametrize("test_data_user_input",["pytorch_invalid_data.txt","pytorch_valid_data.txt"])
+def test_validate_and_render_pytorch(test_data_user_input):
     
-    bad_user_input = {
-        "build": "preview",
-        "os": "win",
-        "package": "piiip",
-        "compute_platform": "ro60"
-    }   
-
+    test_data = test_data_user_input
+    file_name = test_data[0]
+    user_input = test_data[1]
     install = InstallationInstruction.from_file("examples/pytorch/pytorch-instruction.schema.yml.jinja")
 
-    good_installation_instruction = install.validate_and_render(valid_user_input)
+    if "_valid_" in file_name:
+        good_installation_instruction = install.validate_and_render(user_input)
 
-    assert ('Windows does not support ROCm!', True) == good_installation_instruction
+        assert ('Windows does not support ROCm!', True) == good_installation_instruction
+    elif "_invalid_" in file_name:
+        with pytest.raises(Exception):
+            install.validate_and_render(user_input)
 
-    with raises(Exception):
-        install.validate_and_render(bad_user_input)
-
-
-def test_validate_and_render_scikit():
-    valid_user_input = {
-        "os": "Windows",
-        "packager": "pip",
-        "virtualenv": False
-    }
-
-    invalid_user_input = {
-        "os": "Kali",
-        "packager": "pip",
-        "virtualenv": False
-    }
+@pytest.mark.parametrize("test_data_user_input",["scikit_invalid_data.txt","scikit_valid_data.txt"])
+def test_validate_and_render_scikit(test_data_user_input):
+    test_data = test_data_user_input
+    file_name = test_data[0]
+    user_input = test_data[1]
 
     install = InstallationInstruction.from_file("examples/scikit-learn/scikit-learn-instruction.schema.yml.jinja")
 
-    good_installation_instruction = install.validate_and_render(valid_user_input)
+    if "_valid_" in file_name:
+        good_installation_instruction = install.validate_and_render(user_input)
 
-    assert ('pip install -U scikit-learn', False) == good_installation_instruction
+        assert ('pip install -U scikit-learn', False) == good_installation_instruction
 
-    with raises(Exception):
-        install.validate_and_render(invalid_user_input)
+    elif "_invalid_" in file_name:
+        with pytest.raises(Exception):
+            install.validate_and_render(user_input)
