6e99ce3636fcca68da9ffe53323f6c2c4a57226d
TimoEg <TimoEge@online.de>
2024-05-26 13:17:51 +0200

    overworked running  test multible times  with different data

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,20 +17,55 @@ import pytest
 import os
 import yaml
 import json
+import glob
+
+example_schemas = {
+    "pytorch":"examples/pytorch/pytorch-instruction.schema.yml.jinja",
+    "scikit":"examples/scikit-learn/scikit-learn-instruction.schema.yml.jinja",
+    "spacy":"examples/spacy/spacy-instruction.schema.yml.jinja"
+}
+
+_user_input_data = []
+_user_input_name = []
+
+for filename in glob.glob(os.path.join(os.path.split(__file__)[0], "data", "*valid_data.*")):
+    with open(filename, "r") as f:
+        try:
+            with open(filename, "r") as f:
+                _user_input_data.append(json.load(f))
+                _user_input_name.append(os.path.basename(filename))
+        except:
+            with open(filename, "r") as f:
+                split = f.readlines()
+                split[0] = split[0][:-1]
+                _user_input_data.append((split[0],split[1] == 'True'))
+                _user_input_name.append(os.path.basename(filename))
+
+packages = []
+for i in _user_input_name:
+    package = i.split("_")[0]
+    if package not in packages:
+        packages.append(package)
+
+test_data = []
+for package in packages:
+    valid_id = _user_input_name.index(f"{package}_valid_data.json")
+    invalid_id = _user_input_name.index(f"{package}_invalid_data.json")
+    expected_value = _user_input_name.index(f"{package}_expected_valid_data.txt")
+    tested_file_path = example_schemas.get(package)
+    test_data.append([tested_file_path,_user_input_data[valid_id],_user_input_data[invalid_id],_user_input_data[expected_value]])
+
+
+def pytest_generate_tests(metafunc):
+    if "user_input_tests" in metafunc.fixturenames:
+        metafunc.parametrize("user_input_tests",test_data)
 
 
 @pytest.fixture
 def test_data_flags_options():
     file_path = os.path.join(os.path.dirname(__file__), 'data', 'flags_options_example.schema.yml')
     with open(file_path,"r") as file:
-        return yaml.safe_load(file)
-
-@pytest.fixture
-def test_data_user_input(request):
-    file_name = request.param
-    file_path = os.path.join(os.path.dirname(__file__), 'data', file_name)
-    with open(file_path,"r") as file:
-        data = file.read()
-    test_input = json.loads(data)
-    return [file_name,test_input]
+        example = yaml.safe_load(file)
+    
+    return example
 
