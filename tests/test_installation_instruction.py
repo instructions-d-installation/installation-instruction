b74752495a7f0628e7cd99682fdf3c850b4a4701
TimoEg <TimoEge@online.de>
2024-05-26 13:20:34 +0200

    removed seperate spacy test

--- tests/test_installation_instruction.py
+++ tests/test_installation_instruction.py
@@ -15,25 +15,4 @@ def test_validate_and_render(user_input_tests):
     with pytest.raises(Exception):
         install.validate_and_render(invalid_input)
 
-def test_validate_and_render_spacy():
-    valid_user_input = {
-        "os": "windows",
-        "platform": "x86",
-        "package": "pip",
-        "hardware": "cpu"
-    }
 
-    invalid_user_input = {
-        "os": "windows",
-        "platform": "arm",
-        "package": "forge",
-        "hardware": "cpu"
-    }
-
-    install = InstallationInstruction.from_file("examples/spacy/spacy-instruction.schema.yml.jinja")
-
-    good_installation_instruction = install.validate_and_render(valid_user_input)
-    assert ('pip install -U pip setuptools wheel && pip install -U spacy', False) == good_installation_instruction
-
-    with pytest.raises(Exception):
-        install.validate_and_render(invalid_user_input)
